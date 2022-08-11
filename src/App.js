d3c7b49c0e7c810c3020fc8b5ea91fc21dd5d408
katongole89 <katongolerichard089@gmail.com>
2022-08-11 17:17:00 +0300

    added getting the host or client name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 const App = () =>{
+
+    useEffect(()=>{
+        // const parsedData = window.location.protocol + '//' + window.location.host 
+        const parsedData = window.location.host.trim()
+        const siteUrl = parsedData.split('.')[0]
+        console.log(siteUrl)
+    }, [])
+
     return(
-        <span>New app is working</span>
+        <>
+            <span>New app is working</span>
+            {/* <span>{domain}</span> */}
+        </>
+        
     )
 }
 
